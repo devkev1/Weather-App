47ea62d3193f438a1d0e554939bb524057932472
Kevin Wong <kevingywong@gmail.com>
2021-12-09 15:21:34 -0500

    fixed code to remove cards

--- app.js
+++ app.js
@@ -5,84 +5,82 @@ const display = document.querySelector(".display");
 const cities = [];
 
 const submit = () => {
-    const isCity = isNaN(inputValue.value)
-    console.log(isCity ? "It's a city!" : "It's a zip code!");
+  const isCity = isNaN(inputValue.value);
+  console.log(isCity ? "It's a city!" : "It's a zip code!");
 
-    const unit = checkbox.checked ? "metric" : "imperial";
+  const unit = checkbox.checked ? "metric" : "imperial";
 
-    let path = "https://api.openweathermap.org/data/2.5/weather?";
-    if (isCity) {
-      path += "q=";
-    } else {
-      path += "zip=";
-    }
-    path += `${inputValue.value}&appid=188d895134c8507cd1dacc245888d46e&units=${unit}`
-    //`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=188d895134c8507cd1dacc245888d46e&units=${unit}`
-    fetch(path)
-      .then((response) => response.json())
-      .then((data) => {
-        data.metric = checkbox.checked;
+  let path = "https://api.openweathermap.org/data/2.5/weather?";
+  if (isCity) {
+    path += "q=";
+  } else {
+    path += "zip=";
+  }
+  path += `${inputValue.value}&appid=188d895134c8507cd1dacc245888d46e&units=${unit}`;
+  //`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=188d895134c8507cd1dacc245888d46e&units=${unit}`
+  fetch(path)
+    .then((response) => response.json())
+    .then((data) => {
+      data.metric = checkbox.checked;
 
-        if (data.cod === 200) {
-          cities.push(data)
-        } else {
-          throw Error;
-        }
+      if (data.cod === 200) {
+        cities.push(data);
+      } else {
+        throw Error;
+      }
 
-        display.innerHTML = "";
+      display.innerHTML = "";
 
-        for (let i = 0; i < cities.length; i++) {
-            
-            let div = document.createElement("div");
-            div.classList.add("card");
+      for (let i = 0; i < cities.length; i++) {
+        let div = document.createElement("div");
+        div.classList.add("card");
 
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("delete-button");
-            deleteButton.innerText = "X";
-            
-            const HR = document.createElement("HR");
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.innerText = "X";
 
-            const icon = document.createElement("img"); 
-            const name = document.createElement("h2");
-            const desc = document.createElement("p");
-            const temp = document.createElement("p");
-            
-            name.innerHTML = cities[i]["name"];
-            desc.innerHTML = cities[i]["weather"][0]["description"];
-            temp.innerHTML = Math.round(cities[i]["main"]["temp"]) + (cities[i].metric ? " &deg;C" : " &deg;F");
-            icon.src = [
-                "http://openweathermap.org/img/wn/" + cities[i]["weather"][0]["icon"] + ".png"];
+        const HR = document.createElement("HR");
 
-            div.append(HR, name, desc, icon, temp, deleteButton);
-            display.prepend(div);
-        }
+        const icon = document.createElement("img");
+        const name = document.createElement("h2");
+        const desc = document.createElement("p");
+        const temp = document.createElement("p");
 
-        deleteButton.addEventListener("click", removeCard);
+        name.innerHTML = cities[i]["name"];
+        desc.innerHTML = cities[i]["weather"][0]["description"];
+        temp.innerHTML =
+          Math.round(cities[i]["main"]["temp"]) +
+          (cities[i].metric ? " &deg;C" : " &deg;F");
+        icon.src = [
+          "http://openweathermap.org/img/wn/" +
+            cities[i]["weather"][0]["icon"] +
+            ".png",
+        ];
 
-        const removeCard = () => {
-          let display = document.querySelector("div");
+        div.append(HR, name, desc, icon, temp, deleteButton);
+        display.prepend(div);
+
+        const removeCard = (e) => {
+          let display = document.querySelector(".display");
           display.removeChild(div);
 
-          let foundIndex = cities.findIndex((city) => {
-            return city.id == id;
-          });
-          if (foundIndex > - 1) {
-            cities.splice(foundIndex, 1);
-          }
-        }
-        console.log(data);
-      })
-      .catch(() => alert("Something went wrong!"));
-  }
+          cities.splice(i, 1);
+        };
+        deleteButton.addEventListener("click", removeCard);
+      }
+      console.log(data);
+    })
+    .catch(() => alert("Something went wrong!"));
+};
 
 const toggleUnit = () => {
-  const unit = document.getElementById("unit")
+  const unit = document.getElementById("unit");
   if (checkbox.checked) {
-    unit.innerHTML = "&deg;C"
+    unit.innerHTML = "&deg;C";
   } else {
-    unit.innerHTML = "&deg;F"
+    unit.innerHTML = "&deg;F";
   }
-}
+};
 
 button.addEventListener("click", submit);
 
@@ -91,6 +89,3 @@ inputValue.addEventListener("keyup", (e) => {
     submit();
   }
 });
-
-// close card window 
-  
